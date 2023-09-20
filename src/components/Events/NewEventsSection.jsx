8491683caee0fa752069e7affeeafcf82be1ee1c
Gonzalo Borderolle <gborderolle@gmail.com>
2023-09-20 16:26:33 -0300

    Initial commit

---
+++ src/components/Events/NewEventsSection.jsx
@@ -0,0 +1,51 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchEvents } from '../../util/http';
+
+import LoadingIndicator from '../UI/LoadingIndicator.jsx';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+import EventItem from './EventItem.jsx';
+
+export default function NewEventsSection() {
+  // Clase 370: https://www.udemy.com/course/react-the-complete-guide-incl-redux/learn/lecture/39499426#overview
+  // Tanstack Query
+  const { data, isPending, isError, error, refetch } = useQuery({
+    queryKey: ['events'],
+    queryFn: fetchEvents,
+  });
+
+  let content;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock
+        title='An error occurred'
+        message={error.info?.message || 'Falló al traer los eventos.'}
+      />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className='events-list'>
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+  return (
+    <section className='content-section' id='new-events-section'>
+      <header>
+        <h2>Recently added events</h2>
+      </header>
+      {content}
+    </section>
+  );
+}
